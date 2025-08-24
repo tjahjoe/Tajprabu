e27d96ffc3d540d0bc8a5fbab72aeff10c3d3a75
tjahjoe <ntell634@gmail.com>
2025-08-24 12:03:04 +0700

    add: all service and repositories in providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,11 @@ use App\Services\Implementations\TopicService;
 use App\Services\Implementations\RoleService;
 use App\Services\Implementations\UserService;
 use App\Services\Implementations\ArticleService;
+use App\Services\Implementations\ImageService;
+use App\Services\Implementations\LikeService;
+use App\Services\Implementations\CommentService;
+use App\Services\Implementations\NotificationService;
+use App\Services\Implementations\LogService;
 
 use App\Repositories\Interfaces\PosterRepositoryInterface;
 use App\Repositories\Interfaces\TagRepositoryInterface;
@@ -29,6 +34,11 @@ use App\Repositories\Interfaces\TopicRepositoryInterface;
 use App\Repositories\Interfaces\RoleRepositoryInterface;
 use App\Repositories\Interfaces\UserRepositoryInterface;
 use App\Repositories\Interfaces\ArticleRepositoryInterface;
+use App\Repositories\Interfaces\ImageRepositoryInterface;
+use App\Repositories\Interfaces\LikeRepositoryInterface;
+use App\Repositories\Interfaces\CommentRepositoryInterface;
+use App\Repositories\Interfaces\NotificationRepositoryInterface;
+use App\Repositories\Interfaces\LogRepositoryInterface;
 
 use App\Repositories\Implementations\PosterRepository;
 use App\Repositories\Implementations\TagRepository;
@@ -36,6 +46,11 @@ use App\Repositories\Implementations\TopicRepository;
 use App\Repositories\Implementations\RoleRepository;
 use App\Repositories\Implementations\UserRepository;
 use App\Repositories\Implementations\ArticleRepository;
+use App\Repositories\Implementations\ImageRepository;
+use App\Repositories\Implementations\LikeRepository;
+use App\Repositories\Implementations\CommentRepository;
+use App\Repositories\Implementations\NotificationRepository;
+use App\Repositories\Implementations\LogRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -50,6 +65,12 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(RoleServiceInterface::class, RoleService::class);
         $this->app->bind(UserServiceInterface::class, UserService::class);
         $this->app->bind(ArticleServiceInterface::class, ArticleService::class);
+        $this->app->bind(ImageServiceInterface::class, ImageService::class);
+        $this->app->bind(LikeServiceInterface::class, LikeService::class);
+        $this->app->bind(CommentServiceInterface::class, CommentService::class);
+        $this->app->bind(NotificationServiceInterface::class, NotificationService::class);
+        $this->app->bind(LogServiceInterface::class, LogService::class);
+
 
         $this->app->bind(PosterRepositoryInterface::class, PosterRepository::class);
         $this->app->bind(TagRepositoryInterface::class, TagRepository::class);
@@ -57,6 +78,11 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(RoleRepositoryInterface::class, RoleRepository::class);
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
         $this->app->bind(ArticleRepositoryInterface::class, ArticleRepository::class);
+        $this->app->bind(ImageRepositoryInterface::class, ImageRepository::class);
+        $this->app->bind(LikeRepositoryInterface::class, LikeRepository::class);
+        $this->app->bind(CommentRepositoryInterface::class, CommentRepository::class);
+        $this->app->bind(NotificationRepositoryInterface::class, NotificationRepository::class);
+        $this->app->bind(LogRepositoryInterface::class, LogRepository::class);
     }
 
     /**
